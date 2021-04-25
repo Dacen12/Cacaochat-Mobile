6f48665ce05b2677fcbc11e5c2addd7eec6088cc
dacen12 <alkisali1@hotmail.com>
2021-04-25 13:15:14 +0100

    Updated the socket link from simply chat to cacaochat

--- app/src/main/java/com/project/cacaochat/socketConnection/SocketConnection.kt
+++ app/src/main/java/com/project/cacaochat/socketConnection/SocketConnection.kt
@@ -4,7 +4,7 @@ import io.socket.client.IO
 import io.socket.client.Socket
 
 class SocketConnection {
-    private var socket : Socket = IO.socket("http://192.168.1.175:3000/")
+    private var socket : Socket = IO.socket("https://cacaochat.herokuapp.com/")
 
    fun getSocket(): Socket {
        return socket
